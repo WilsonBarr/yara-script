b74e1f97f832a55bc406fe8effd3602132c30736
Wilson Barr <wbarr@redhat.com>
2022-08-04 16:40:06 -0400

    updated script to pull from quay with arguments, defaulting to scanning local dockerfile

--- yara_hunter_check.sh
+++ yara_hunter_check.sh
@@ -7,13 +7,35 @@ docker pull $YARA_HUNTER_IMAGE
 
 # build image from Dockerfile for scanning
 
-docker build -f Dockerfile -t image_name .
+if [ $# -eq 0 ]# for no arguments
+  then
+    echo "About to Scan Dockerfile image"
+    docker build -f Dockerfile -t image_name .
+    docker run -it --rm --name=deepfence-yarahunter \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    $YARA_HUNTER_IMAGE --image-name image_name
+    echo "Finished Scanning Dockerfile image"
+    docker rmi image_name
+    docker rmi $YARA_HUNTER_IMAGE
+else # for arguments if they exist, format dir/name:tag
+    for thing in "$@"
+    do
+        echo "About to Scan $thing"
+        docker pull "quay.io/$thing"
+        docker run -it --rm --name=deepfence-yarahunter \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        $YARA_HUNTER_IMAGE --image-name "quay.io/$thing"
+        echo "Finished Scanning $thing"
+        docker rmi "quay.io/$thing"
+        docker rmi $YARA_HUNTER_IMAGE
+    done
+fi
 
 # scan regularly, results not put in json 
 
-docker run -it --rm --name=deepfence-yarahunter \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    $YARA_HUNTER_IMAGE --image-name image_name 
+# docker run -it --rm --name=deepfence-yarahunter \
+#    -v /var/run/docker.sock:/var/run/docker.sock \
+#    $YARA_HUNTER_IMAGE --image-name image_name 
 
 # scan & add results to json, puts results into /my-output/node-scan.json
 
@@ -24,7 +46,6 @@ docker run -it --rm --name=deepfence-yarahunter \
 #    --json-filename node-scan.json
 
 # remove images 
-docker rmi image_name
-docker rmi $YARA_HUNTER_IMAGE
+
 
 
